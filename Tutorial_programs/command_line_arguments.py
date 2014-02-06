a39c61f2918b41077598b034980bc3723407aa2b
Abhinav Garg <abhinav.garg@live.com>
2014-02-06 19:04:31 +0530

    Adding a Tutorial_programs folder, to keep track of concepts learnt along the way

---
+++ Tutorial_programs/command_line_arguments.py
@@ -0,0 +1,8 @@
+import sys
+
+'''Learn about passing command line arguments'''
+'''sys.argv is a list of command line arguments passed'''
+'''sys.argv[0] is the name of the script being run'''
+
+for el in sys.argv:
+	print el
