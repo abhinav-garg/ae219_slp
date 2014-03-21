7798e0574a1efdf9c6372aaaaa9a121d76ffc998
Abhinav Garg <abhinav.garg@live.com>
2014-03-21 12:42:11 +0530

    Better Test Sample, python code modified to take 3D

---
+++ NNP_Python_3d.py
@@ -0,0 +1,81 @@
+import sys, pickle
+
+def within_distance(a, b, radius):
+	'''Checks if within radius, saves on square root operation'''
+	# print "CHECKPOINT Distance function called"
+	square_distance = (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2
+	if square_distance <= radius ** 2:
+		return True
+	# print "CHECKPOINT Distance function passed"
+	return False
+
+def nearest_neighbours_list(xMax, yMax, zMax, radius, locations):
+	'''Returns a list of nearest neighbours for each particle (indices for each index)'''
+	cell_0_max = int(xMax // radius + 1)
+	cell_1_max = int(yMax // radius + 1)
+	cell_2_max = int(zMax // radius + 1)
+		# Cells with corresponding particles
+	cellList = [ [[ [] for z in range(0, cell_2_max)] for y in range(0, cell_1_max)] for x in range(0, cell_0_max)]
+		# Generates a 3D list
+
+		# Store corresponding particles in cells
+	for index, particle in enumerate(locations):
+		cellList[int(particle[0] // radius)][int(particle[1] // radius)][int(particle[2] // radius)].append(index)
+		
+		# particles with corresponding cells
+	particle_cell = []
+		# The list is inherently indexed. Just store corresponding locations
+	for index, particle in enumerate(locations):
+		
+		particle_cell.append((int(particle[0] // radius), int(particle[1] // radius), int(particle[2] // radius)))
+	
+	# print cellList	
+		# Neighbours corresponding to each particle
+	neighbours = [[] for x in range(0, len(locations))]	# Initialize
+
+
+
+	for index, particle in enumerate(locations):
+		current_cell = particle_cell[index]		# Tuple
+		for near_0 in [-1, 0, 1]:
+			for near_1 in [-1, 0, 1]:
+				for near_2 in [-1, 0, 1]:
+					if current_cell[0] + near_0 >= 0 and current_cell[0] + near_0 < cell_0_max:
+						if current_cell[1] + near_1 >= 0 and current_cell[1] + near_1 < cell_1_max:
+							if current_cell[2] + near_2 >= 0 and current_cell[2] + near_2 < cell_2_max:
+								for other_particle_index in cellList[current_cell[0] + near_0][current_cell[1] + near_1][current_cell[2] + near_2]:
+									if within_distance(particle, locations[other_particle_index], radius):
+										# print "Entering loop"
+										neighbours[index].append(other_particle_index)
+	return neighbours
+
+def main():
+	xMax = 10.0
+	yMax = 10.0
+	zMax = 10.0
+	radius = 4.0
+	locations = []
+	MAX = 10
+	count = 0
+
+	testSampleFile = open("testSampleFile.txt", "r")
+
+	for string in testSampleFile:
+		coords = string.split(',')
+		coords = [float(el) for el in list(coords)]
+		# print tuple(coords)
+		locations.append(tuple(coords))
+
+		count += 1
+		if count >= MAX:
+			break;
+
+	testSampleFile.close()
+	print locations
+	print nearest_neighbours_list(xMax, yMax, zMax, radius, locations)
+
+if __name__ == '__main__':
+	try:
+		main()
+	except Exception as e:
+		print e
--- NNP_test_sample.py
+++ NNP_test_sample.py
@@ -2,17 +2,22 @@ import random
 import pickle
 import sys
 
-def generate_test(length):
+def generateTestSample(length):
 	random.seed(3)
-	test = [random.random() * 10 for el in range(0, int(length))]
+	testSample = [(random.random() * 10, random.random() * 10, random.random() * 10) for el in range(0, int(length))]
 	# print test
-	pickle.dump(test, open('NNP_test_' + str(length) + '.p', 'wb'))
+	testSampleFile = open("testSampleFile.txt", "w")
+	string = ""	# Initialize an empty string
+	for testPoint in testSample:
+		string += str(testPoint[0]) + "," + str(testPoint[1]) + "," + str(testPoint[2]) + "\n"
+	testSampleFile.write(string)
+	testSampleFile.close()
 
 if __name__ == '__main__':
 	try:
 		print 'Populating a sample of ' + str(sys.argv[1]) + ' elements'
-		generate_test(sys.argv[1])
-		print 'Dumping the list in the file NNP_test_' + str(sys.argv[1])
+		generateTestSample(sys.argv[1])
+		print 'Writing the list as a CSV file to the file testSampleFile.txt'
 	except Exception as e:
 		print e 
 		print 'Pass the size of the sample as an argument'
